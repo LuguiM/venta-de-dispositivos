86c0bd6489ce8108fe6795df57cc40ce623c37a8
Estela25 <USIS035821@ugb.edu.sv>
2021-10-10 16:26:47 -0600

    cambios

--- fusuario.Designer.cs
+++ fusuario.Designer.cs
@@ -153,27 +153,27 @@ namespace venta_de_dispositivos
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(222, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 10;
-            this.label1.Text = "label1";
+            this.label1.Text = "Usuario";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(225, 93);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 11;
-            this.label2.Text = "label2";
+            this.label2.Text = "Password";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(225, 151);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 12;
-            this.label3.Text = "label3";
+            this.label3.Text = "Nivel";
             // 
             // pictureBox1
             // 
